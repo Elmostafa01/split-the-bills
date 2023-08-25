4d51bbd6b471470aec5f364c727fcdb9dac99d57
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-08-25 05:13:36 +0100

    pushing finished project

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import hero from '../images/Hero.svg'
+
+const LandingPage = () => {
+    const navigate = useNavigate();
+
+    const goToSplitPage = () => {
+        navigate('/split')
+    }
+  return (
+    <div className='lp'>
+      <div className='main'>
+        <div className="hero">
+            <img src={hero}/>
+        </div>
+        <div className='text-p'>
+            <p>Streamline bill splitting with ease. Our app simplifies sharing expenses among friends, ensuring fairness and simplicity. Enjoy moments together without the hassle.</p>
+        </div>
+        <div className='btn'>
+            <button onClick={goToSplitPage}>Try it Now!</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
